c2dd60e6615d1d8ebfd30fe41c3d75f7cc4bba24
MarvinD00 <marvin.diedrich@gmx.de>
2023-10-21 14:41:20 +0200

    LearningAgent: add testscript
    The script.py will be our future main script.
    Right now it will be used for testing.
    This commit adds simple code to test the movement of a
    tetrimino and collision detection as well as player movement.

---
+++ bin/script.py
@@ -0,0 +1,44 @@
+# Example file showing a circle moving on screen
+import pygame
+import random
+import src.Objects
+import src.controllers
+from src.Objects import Tetrimino as Tetrimino
+from src.controllers import TetriminoController as TetriminoController
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Tetris Learner")
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+# create a tetrimino
+tetrimino = Tetrimino.Tetrimino(0, 0)
+# create a tetrimino controller
+tetrimino_controller = TetriminoController.TetriminoController(
+    tetrimino, screen)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # every second move down
+    tetrimino_controller.move_down()
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
