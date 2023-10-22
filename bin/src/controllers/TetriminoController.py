8bf1fcbe7ea3cbf789e53d572f272cc6d2410eda
MarvinD00 <marvin.diedrich@gmx.de>
2023-10-22 09:10:18 +0200

    LearningAgent: Changes to tetriminoController
    1. No need for tetrimino param the controller just generates new ones now
       on creation and on function call new_tetrimino
    2. new STOP_MOVE_EVENT to show that a tetrimino has landed and a new one
       needs to be created
    3. is valid move has been changed to
    	1. correctly check if tetrimino would move out of bounds
    	2. correctly check if a tetrimino will land on lower side of
    	   the window or another tetrimino
    4. update block grid is now called and only checks for old tetriminos
       because we dont want the current tetrimino to block itself
    5. new draw function in tetrimino controller that draws
    	1. the current tetrimino
    	2. all old tetriminos

--- bin/src/controllers/TetriminoController.py
+++ bin/src/controllers/TetriminoController.py
@@ -1,15 +1,18 @@
 import pygame
 from ..Objects import Tetrimino as Tetrimino
 
+STOP_MOVE_EVENT = pygame.USEREVENT + 1
+
 
 class TetriminoController:
 
-    def __init__(self, tetrimino, screen):
+    def __init__(self, screen, tetrimino=Tetrimino.Tetrimino(0, 0)):
         self.tetrimino = tetrimino
         self.screen = screen
         self.last_fall_time = pygame.time.get_ticks()
         self.fall_interval = 250
         self.block_grid = []
+        self.landed_tetriminos = []
 
         # Initialize the block grid
         for i in range(self.screen.get_height() // self.tetrimino.block_size):
@@ -17,17 +20,15 @@ class TetriminoController:
                 [None] * (self.screen.get_width() // self.tetrimino.block_size))
 
     def move_left(self):
-        self.tetrimino.draw(self.screen)
         if all(self.is_valid_move(block.rect.x - self.tetrimino.block_size, block.rect.y) for block in self.tetrimino.blocks):
             self.tetrimino.move("left")
 
     def move_right(self):
-        self.tetrimino.draw(self.screen)
         if all(self.is_valid_move(block.rect.x + self.tetrimino.block_size, block.rect.y) for block in self.tetrimino.blocks):
             self.tetrimino.move("right")
 
     def move_down(self):
-        self.tetrimino.draw(self.screen)
+        self.update_block_grid()
         if all(self.is_valid_move(block.rect.x, block.rect.y + self.tetrimino.block_size) for block in self.tetrimino.blocks):
             current_time = pygame.time.get_ticks()
             if current_time - self.last_fall_time >= self.fall_interval:
@@ -38,16 +39,18 @@ class TetriminoController:
     def is_valid_move(self, x, y):
         # Check if the move would cause a collision with the boundaries or other blocks
         for block in self.tetrimino.blocks:
+            if (x >= self.screen.get_width()):
+                return False
+            if (y >= self.screen.get_height()
+                    or self.block_grid[y // self.tetrimino.block_size][x // self.tetrimino.block_size] is not None
+                    ):
+                print("landed")
+                pygame.event.post(pygame.event.Event(STOP_MOVE_EVENT))
+                return False
             if (
                 x < 0
-                or x >= self.screen.get_width()
-                or y >= self.screen.get_height()
-                or y < 0
-                or self.block_grid[y // self.tetrimino.block_size][x // self.tetrimino.block_size] is not None
             ):
                 return False
-            print("valid move")
-            print("current position: ", x, y)
         return True
 
     def update_block_grid(self):
@@ -58,7 +61,17 @@ class TetriminoController:
                     self.block_grid[row][col] = None
 
         # Update the game grid with the new position of the Tetrimino's blocks
-        for block in self.tetrimino.blocks:
-            row = block.rect.y // self.tetrimino.block_size
-            col = block.rect.x // self.tetrimino.block_size
-            self.block_grid[row][col] = block
+        for tetrimino in self.landed_tetriminos:
+            for block in tetrimino.blocks:
+                row = block.rect.y // self.tetrimino.block_size
+                col = block.rect.x // self.tetrimino.block_size
+                self.block_grid[row][col] = block
+
+    def new_tetrimino(self):
+        self.landed_tetriminos.append(self.tetrimino)
+        self.tetrimino = Tetrimino.Tetrimino(0, 0)
+
+    def draw(self):
+        self.tetrimino.draw(self.screen)
+        for tetrimino in self.landed_tetriminos:
+            tetrimino.draw(self.screen)
