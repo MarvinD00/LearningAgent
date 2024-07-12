bece1f093d2a77c4ef6ce2c9410e170a115cb9a5
MarvinD00 <marvin.diedrich@gmx.de>
2024-07-12 10:34:54 +0200

    change indentation to tabs in GameController
    because i forgot and it bothers me

--- bin/src/controllers/GameController.py
+++ bin/src/controllers/GameController.py
@@ -4,77 +4,77 @@ from . import TetriminoController
 
 class GameController:
 
-    def __init__(self, screen):
-        self.screen = screen
-        self.tetrimino_controller = TetriminoController.TetriminoController(screen)
-        self.dt = 0
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.score = 0
-        self.run()
+	def __init__(self, screen):
+		self.screen = screen
+		self.tetrimino_controller = TetriminoController.TetriminoController(screen)
+		self.dt = 0
+		self.clock = pygame.time.Clock()
+		self.running = True
+		self.score = 0
+		self.run()
 
-    def run(self):
-        while self.running:
-            # poll for events
-            # pygame.QUIT event means the user clicked X to close your window
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT or event.type == TetriminoController.END_GAME_EVENT:
-                    return False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_a:
-                        self.tetrimino_controller.move_left()
-                    elif event.key == pygame.K_d:
-                        self.tetrimino_controller.move_right()
-                    elif event.key == pygame.K_LEFT:
-                        self.tetrimino_controller.move_left()
-                    elif event.key == pygame.K_RIGHT:
-                        self.tetrimino_controller.move_right()
-                    elif event.key == pygame.K_s:
-                        self.tetrimino_controller.move_down()
-                        self.score += 5
-                    elif event.key == pygame.K_DOWN:
-                        self.tetrimino_controller.move_down()
-                        self.score += 5
-                    elif event.key == pygame.K_w:
-                        self.tetrimino_controller.rotate()
-                    elif event.key == pygame.K_UP:
-                        self.tetrimino_controller.rotate()
-                if event.type == TetriminoController.STOP_MOVE_EVENT:
-                    self.tetrimino_controller.new_tetrimino()
-                if event.type == TetriminoController.TETRIS_EVENT:
-                    self.score += 100
-                if event.type == TetriminoController.TETRIS_COMBO_DOUBLE_EVENT:
-                    self.score += 250
-                if event.type == TetriminoController.TETRIS_COMBO_TRIPLE_EVENT:
-                    self.score += 400
-                if event.type == TetriminoController.TETRIS_COMBO_QUADRUPLE_EVENT:
-                    self.score += 600
+	def run(self):
+		while self.running:
+			# poll for events
+			# pygame.QUIT event means the user clicked X to close your window
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT or event.type == TetriminoController.END_GAME_EVENT:
+					return False
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_a:
+						self.tetrimino_controller.move_left()
+					elif event.key == pygame.K_d:
+						self.tetrimino_controller.move_right()
+					elif event.key == pygame.K_LEFT:
+						self.tetrimino_controller.move_left()
+					elif event.key == pygame.K_RIGHT:
+						self.tetrimino_controller.move_right()
+					elif event.key == pygame.K_s:
+						self.tetrimino_controller.move_down()
+						self.score += 5
+					elif event.key == pygame.K_DOWN:
+						self.tetrimino_controller.move_down()
+						self.score += 5
+					elif event.key == pygame.K_w:
+						self.tetrimino_controller.rotate()
+					elif event.key == pygame.K_UP:
+						self.tetrimino_controller.rotate()
+				if event.type == TetriminoController.STOP_MOVE_EVENT:
+					self.tetrimino_controller.new_tetrimino()
+				if event.type == TetriminoController.TETRIS_EVENT:
+					self.score += 100
+				if event.type == TetriminoController.TETRIS_COMBO_DOUBLE_EVENT:
+					self.score += 250
+				if event.type == TetriminoController.TETRIS_COMBO_TRIPLE_EVENT:
+					self.score += 400
+				if event.type == TetriminoController.TETRIS_COMBO_QUADRUPLE_EVENT:
+					self.score += 600
 
-            # fill the screen with a color to wipe away anything from last frame
-            self.screen.fill("purple")
+			# fill the screen with a color to wipe away anything from last frame
+			self.screen.fill("purple")
 
-            # every second increase score by 10 and move down
-            self.dt += 1
-            if self.dt >= 60:
-                self.score += 10
-                self.dt = 0
-                self.tetrimino_controller.move_down()
-                self.label = self.myfont.render(
-                    "Score: " + str(self.score), 1, (255, 255, 0))
-            # draw label
-            # render text
-            self.myfont = pygame.font.Font(None, 30)
-            self.label = self.myfont.render(
-                f"Score: {self.score}", 1, (255, 255, 0))
-            self.screen.blit(self.label, (0, 0))
+			# every second increase score by 10 and move down
+			self.dt += 1
+			if self.dt >= 60:
+				self.score += 10
+				self.dt = 0
+				self.tetrimino_controller.move_down()
+				self.label = self.myfont.render(
+					"Score: " + str(self.score), 1, (255, 255, 0))
+			# draw label
+			# render text
+			self.myfont = pygame.font.Font(None, 30)
+			self.label = self.myfont.render(
+				f"Score: {self.score}", 1, (255, 255, 0))
+			self.screen.blit(self.label, (0, 0))
 
-            # every second move down and draw
-            self.tetrimino_controller.draw()
+			# every second move down and draw
+			self.tetrimino_controller.draw()
 
-            # flip() the display to put your work on screen
-            pygame.display.flip()
+			# flip() the display to put your work on screen
+			pygame.display.flip()
 
-            # limits FPS to 60
-            # dt is delta time in seconds since last frame, used for framerate-
-            # independent physics.
-            dt = self.clock.tick(60) / 1000
+			# limits FPS to 60
+			# dt is delta time in seconds since last frame, used for framerate-
+			# independent physics.
+			dt = self.clock.tick(60) / 1000
